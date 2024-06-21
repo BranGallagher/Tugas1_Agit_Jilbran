bb056cc9019ebea2949fbbdaeb45e81910bf0b00
BranGallagher <m.jlbrn21@gmail.com>
2024-06-21 15:43:20 +0700

    Tugas Agit

---
+++ src/main/java/id/co/astratech/tugas1_agit_muhamadJilbran/Tugas1AgitMuhamadJilbranApplication.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugas1_agit_muhamadJilbran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1AgitMuhamadJilbranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tugas1AgitMuhamadJilbranApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/astratech/tugas1_agit_muhamadJilbran/Tugas1AgitMuhamadJilbranApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugas1_agit_muhamadJilbran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas1AgitMuhamadJilbranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
